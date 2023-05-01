ab3c1263cdc93ea7fec898b856879b82282d5054
Philipp <philippkotte@icloud.com>
2023-05-01 13:49:39 +0200

    Initial Commit

---
+++ Logbuch/LogbuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogbuchApp.swift
+//  Logbuch
+//
+//  Created by Philipp Kotte on 01.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LogbuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
